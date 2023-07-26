db58411dbd3866575f2b114357983f2139c6a801
MohamedElsoudy <mohamedelsoudy373@gmail.com>
2023-07-26 18:40:33 +0200

    updating home

---
+++ js/main.js
@@ -0,0 +1,9 @@
+ 
+//  / hidden user notofication
+
+const hidBtn=document.getElementById('icon-hidden');
+const messageNot=document.getElementById('messages');
+ 
+hidBtn.addEventListener('click',()=>{
+    messageNot.style.display='none'
+})
